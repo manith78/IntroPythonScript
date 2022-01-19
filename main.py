eacef648cb8c8ec3cc33b5826f9d0b517bb97ce6
manithm <56009388+manith78@users.noreply.github.com>
2022-01-19 02:01:42 +0000

    Added a new line to check Git repo and Git hub sync

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ print(calculate.x)
 #Added this new line after inital commit
 print(calculate.z) 
 
+#Added this new line after linking with cloud based git hub
+
+print("testing to check if sync with git repo and git hub")
